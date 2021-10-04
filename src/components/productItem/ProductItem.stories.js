634bf5e9dccaa3787f057721a69b6f404bf895f7
tlatmdxo123 <tmdxosmswkd@gmail.com>
2021-10-04 23:22:02 +0900

    styling ProductItem Component

---
+++ src/components/productItem/ProductItem.stories.js
@@ -0,0 +1,18 @@
+import ProductItem from './index'
+
+export default {
+  component: ProductItem,
+  title: "ProductItem",
+};
+
+const info = {
+  image:
+    "https://dnvefa72aowie.cloudfront.net/origin/article/202110/8B65A551506A9E9B9412357C3E32BBE6FF09C8BF9FBD132F59AF7DCC85632085.jpg?q=82&s=300x300&t=crop",
+  title: "삼성 냉장고",
+  address: "서울 강북구 번3동",
+  price:180000,
+  like: 4,
+  chat: 2,
+};
+
+export const Default = () => <ProductItem info={info}></ProductItem>;
---
+++ src/components/productItem/index.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import styled from 'styled-components'
+
+type ProductInfo = {
+    image:string,
+    title:string,
+    price:number,
+    address:string,
+    like:number,
+    chat:number
+}
+
+function ProductItem({info}:{info:ProductInfo}) {
+    const {image,title,price,address,like,chat} = info
+    return (
+        <StyledItem>
+            <div className='product-image'>
+                <img src={image} alt={`${title} image`}/>
+            </div>
+            
+            <div className="product-info">
+                <figcaption className='title'>{title}</figcaption>
+                <figcaption className='price'>{`${price}원`}</figcaption>
+                <figcaption className='address'>{address}</figcaption>
+                <figcaption className='sub'>
+                    <span>{`관심${like}`}</span>
+                    <span>{`채팅${chat}`}</span>
+                </figcaption>
+            </div>
+            
+        </StyledItem>
+    );
+}
+
+const StyledItem = styled.figure`
+    width: 201px;
+    
+    .product-image{
+        width: 100%;
+        height:201px;
+        overflow: hidden;
+        text-align: center;
+        border-radius: 12px;
+        margin-bottom: 12px;
+        img{
+            width: 100%;
+        }
+    }
+    .title{
+        font-size: 16px;
+        margin-bottom: 8px;
+    }
+    .price{
+        font-size: 15px;
+        font-weight: bold;
+        margin-bottom: 6px;
+    }
+    .address{
+        font-size: 13px;
+        margin-bottom: 4px;
+    }
+    .sub{
+        font-size: 13px;
+        color:${props => props.theme.textColorGray};
+        span:first-child{
+            margin-right: 10px;
+        }
+    }
+
+`
+
+export default ProductItem;
