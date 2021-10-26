ca3929c62e6419a48be211925f2d658f87e840e2
tlatmdxo123 <tmdxosmswkd@gmail.com>
2021-10-26 22:34:38 +0900

    기능추가 : 제품리스트 데이터 불러오기

--- src/components/photoInput/index.tsx
+++ src/components/photoInput/index.tsx
@@ -2,32 +2,28 @@ import React from 'react';
 import styled from 'styled-components'
 import {MdCancel,MdOutlineAddAPhoto} from 'react-icons/md'
 
-function PhotoInput() {
-    const imageLists = Array.from({length:10}).map((_) => {
-        return "https://dnvefa72aowie.cloudfront.net/origin/article/202110/8B65A551506A9E9B9412357C3E32BBE6FF09C8BF9FBD132F59AF7DCC85632085.jpg?q=82&s=300x300&t=crop";
-    })
+function PhotoInput({imageLists,onChange,removeFile,name}:{imageLists:string[],onChange:(e:React.ChangeEvent<HTMLInputElement>) => void,removeFile:(target:number) => void,name:string}) {
     return (
-        <ImageForm action='#' method='get'>
-            
+        <ImageFormContainer>
             <div className="mutiple-container">
                 <label htmlFor='input-mutiple-image' className='label box'>
                     <MdOutlineAddAPhoto className='icon'/>
                     <span className='count'>{imageLists.length}/10</span>
                 </label>
-                <input type="file" id='input-mutiple-image' className='image_input' multiple/>
+                <input name={name} type="file" id='input-mutiple-image' className='image_input' onChange={(e) => onChange(e)} multiple/>
                 {imageLists.map((image,index) => (
                     <div className="image box" key={index}>
                         <img src={image}/>
-                        <button className='close'><MdCancel/></button>
+                        <button className='close' onClick={()=>removeFile(index)}><MdCancel/></button>
                     </div>
                 ))}
                 
             </div>
-        </ImageForm>
+        </ImageFormContainer>
     );
 }
 
-const ImageForm = styled.form`
+const ImageFormContainer = styled.div`
     .mutiple-container{
         display: flex;
         flex-wrap: wrap;
--- src/components/productLists/index.tsx
+++ src/components/productLists/index.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import { ProductInfo } from '../../types';
 import ProductItem from '../productItem';
-import UserInfo from '../userInfo';
 import styled from 'styled-components'
+import { User } from '../../store/user/types';
 
-function ProductLists({productDataLists}:{productDataLists:ProductInfo[]}) {
-    const userInfo:UserInfo = {
-        _id:'sdfasdf',
-        name:'심승태',
-        address:"강북구 번3동",
-        profile:"https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Clipart.png"
-    }
+function ProductLists({user,productDataLists}:{user:User,productDataLists:ProductInfo[]}) {
     return (
         <Container>
-            {productDataLists.map((productData,idx) => {
+            {productDataLists && productDataLists.map((productData,idx) => {
                 return (
                     <ItemContainer key={idx} >
-                        <ProductItem image={productData.images[0]} title={productData.title} price={productData.price} address={userInfo.address} chat={productData.chat} like={productData.like}/>
+                        <ProductItem image={productData.images[0]} title={productData.title} price={productData.price} address={productData.address} chat={productData.chat} like={productData.like}/>
                     </ItemContainer>
                 )
             })}
