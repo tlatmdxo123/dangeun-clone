da49e6be44a9d6a81a6fd79bb6eacf2ee6cc36b0
tlatmdxo123 <tmdxosmswkd@gmail.com>
2021-10-29 18:38:00 +0900

    refactor: 로고에 메인페이지 라우팅 추가

--- src/components/common/Logo/index.tsx
+++ src/components/common/Logo/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Image from 'next/image'
+import { useRouter } from 'next/router';
 
 function Logo() {
+    const router = useRouter();
     return (
-        <div className='logo'>
+        <div className='logo' onClick={() => router.push('/')}>
            <Image src='/images/logo.svg' alt='당근마켓 로고' width='120' height='40'/> 
         </div>
     );
